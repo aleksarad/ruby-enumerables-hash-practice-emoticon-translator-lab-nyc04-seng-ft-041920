38d7c126eebf4e275b327bbd606ac54b24d5134f
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-29 19:25:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file, emoticon)
   emoticon_hash = load_library(file)
   emoticon_hash.each do |emotion, eng_jap|
     if emoticon == emoticon_hash[emotion][:english]
-      return emoticon_hash[emotion][:japanese]
+      p emoticon_hash[emotion][:japanese]
     end
   end
   return "Sorry that emoticon was not found"
