15fc3f17375a55523d40393382a49acbc415b3f9
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-29 19:22:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,21 +34,3 @@ get_japanese_emoticon("./lib/emoticons.yml", ":'(")
 def get_english_meaning
   # code goes here
 end
-
-
-# emoticonz = {"angel"=>{:english=>"O:)", :japanese=>"☜(⌒▽⌒)☞"},
-# "angry"=>{:english=>">:(", :japanese=>"ヽ(ｏ`皿′ｏ)ﾉ"},
-# "bored"=>{:english=>":O", :japanese=>"(ΘεΘ;)"},
-# "confused"=>{:english=>"%)", :japanese=>"(゜.゜)"},
-# "embarrassed"=>{:english=>":$", :japanese=>"(#^.^#)"},
-# "fish"=>{:english=>"><>", :japanese=>">゜))))彡"},
-# "glasses"=>{:english=>"8D", :japanese=>"(^0_0^)"},
-# "grinning"=>{:english=>"=D", :japanese=>"(￣ー￣)"},
-# "happy"=>{:english=>":)", :japanese=>"(＾ｖ＾)"},
-# "kiss"=>{:english=>":*", :japanese=>"(*^3^)/~☆"},
-# "sad"=>{:english=>":'(", :japanese=>"(Ｔ▽Ｔ)"},
-# "surprised"=>{:english=>":o", :japanese=>"o_O"},
-# "wink"=>{:english=>";)", :japanese=>"(^_-)"}
-# }
-
-# p emoticonz["angel"][:english]
